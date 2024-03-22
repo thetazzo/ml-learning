5d5600a89d7451367eab50cd84c33f5e6b300072
pwnker <tilen.okretic@gmail.com>
2024-03-23 00:19:16 +0100

    feat: rendering videos with neural netowkrs (session 12)

--- session_12/img2nn.c
+++ session_12/img2nn.c
@@ -1,6 +1,14 @@
 #include <assert.h>
 #include <float.h>
+#include <math.h>
 #include <stdio.h>
+#include <errno.h>
+
+// posix specific headers
+// allows forking childs in linux
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "raylib.h"
 #include "stb_image.h"
@@ -111,6 +119,134 @@ void nnf_plot_cost(NNF_Cost_Plot plot, int rx, int ry, int rw, int rh)
         DrawText("0", rx-rh*0.03f, ry+rh+2, rh*0.03f, RAYWHITE);
     }
 }
+
+#define STR2(x) #x
+#define STR(x) STR2(x)
+ 
+#define out_width 256
+#define out_height 256
+#define FPS 30
+uint32_t out_pixles[out_width*out_height];
+
+void nnf_render_single_frame(NF_NN nn, float img_index)
+{
+    for (int y = 0; y < out_height; ++y) {
+        for (int x = 0; x < out_width; ++x) {
+            NF_MAT_AT(NF_NN_INPUT(nn), 0, 0) = (float)x/(out_width - 1);;
+            NF_MAT_AT(NF_NN_INPUT(nn), 0, 1) = (float)y/(out_height - 1);
+            NF_MAT_AT(NF_NN_INPUT(nn), 0, 2) = img_index;
+            nf_nn_forward(nn);
+            float activation = NF_MAT_AT(NF_NN_OUTPUT(nn), 0, 0);
+            if (activation < 0) activation = 0;
+            if (activation > 1) activation = 1;
+            uint32_t bright = activation*255.f;
+            uint32_t pixel = 0xFF000000|bright|(bright<<8)|(bright<<16);
+            out_pixles[y*out_width + x] = pixel;
+        }
+    }
+}
+
+int nnf_render_upscaled_screenshot(NF_NN nn, float img_index, const char *out_file_path)
+{
+    assert(out_pixles != NULL);
+    nnf_render_single_frame(nn, img_index);
+    if (!stbi_write_png(out_file_path, out_width, out_height, 4, out_pixles, out_width*sizeof(*out_pixles))) {
+        fprintf(stderr, "ERROR: could not write image %s\n", out_file_path);
+        return 1;
+    }
+    printf("Generated %s\n", out_file_path);
+    return 0;
+}
+
+#define READ_END 0
+#define WRITE_END 1
+
+int nnf_render_upscaled_video(NF_NN nn, float duration, const char *out_file_path)
+{
+    // connecting two processes with a pipe ~ unidirectional pipe
+    int pipefd[2];
+    if (pipe(pipefd) < 0) {
+        fprintf(stderr, "ERROR: could not create a pipe: %s\n", strerror(errno));
+        return 1;
+    }
+    // Fork the current process
+    pid_t child = fork();
+    // if child pid is negative it means that the child process was not created
+    if (child < 0) {
+        fprintf(stderr, "ERROR: could not fork a child: %s\n", strerror(errno));
+        return 1;
+    }
+    // if you are the child process pid is equal to 0
+    if (child == 0) {
+        // replace the stdinput with the read end of the pipe
+        if (dup2(pipefd[READ_END], STDIN_FILENO) < 0) {
+            fprintf(stderr, "ERROR: could not reopen read end of the pipe as stdin: %s\n", strerror(errno));
+            return 1;
+        }
+        close(pipefd[WRITE_END]);
+
+        int ret = execlp("ffmpeg",
+                         "ffmpeg",
+                         "-loglevel", "verbose",
+                         "-y",
+                         "-f", "rawvideo",
+                         "-pix_fmt", "rgb32",
+                         "-s", STR(out_width) "x" STR(out_height),
+                         "-r", STR(FPS),
+                         "-an",
+                         "-i", "-", 
+                         "-c:v", "libx264",
+                         out_file_path,
+                         NULL
+                         );
+        if (ret < 0) {
+            fprintf(stderr, "ERROR: could not run ffmpeg as a child process: %s\n", strerror(errno));
+            return 1;
+        }
+        assert(0 && "unreachable");
+    }
+
+    close(pipefd[READ_END]);
+
+    typedef struct {
+        float start;
+        float end;
+    } Segment;
+    
+    Segment segments[] = {
+        {0, 0},
+        {0, 1},
+        {1, 1},
+        {1, 0},
+    };
+
+    size_t segments_count = NF_ARRAY_LEN(segments);
+    float segment_length = 1.f/segments_count;
+
+    // render video
+    size_t frame_count = FPS*duration;
+    for (size_t i = 0; i < frame_count; ++i) {
+        float img_index = (float)i/frame_count;
+        
+        // easing animation
+        size_t segment_index = floorf(img_index/segment_length);
+        float segment_porgress = img_index/segment_length - segment_index;
+        if (segment_index > segments_count) segment_index = segments_count - 1;
+        Segment segment = segments[segment_index];
+        float a = segment.start + (segment.end - segment.start)*sqrtf(segment_porgress);
+        nnf_render_single_frame(nn, a);
+        write(pipefd[WRITE_END], out_pixles, sizeof(*out_pixles)*out_width*out_height); 
+    }
+
+    close(pipefd[WRITE_END]);
+
+    // wait for the child to finish executing
+    wait(NULL);
+
+    printf("Generated %s\n", out_file_path);
+    return 0;
+}
+
 char *p2m_shift_args(int *argc, char ***argv)
 {
     assert(*argc > 0);
@@ -261,7 +397,7 @@ int main(int argc, char **argv)
     size_t batch_count = (td.rows + batch_size-1)/batch_size;
     size_t batch_begin = 0;
     float cost = 0.f;
-    float rate = 0.5f;
+    float rate = 1.f;
     bool isRunning = false;
     float preview_scroll = 0.5f;
     bool preview_scroll_dragging = false;
@@ -283,28 +419,12 @@ int main(int argc, char **argv)
 
         // screenshot neural network based on preview scroll 
         if (IsKeyPressed(KEY_S)) {
-            int out_width = 512;
-            int out_height = 512;
-            uint8_t *out_pixles = malloc(sizeof(*out_pixles)*out_width*out_height);
-            assert(out_pixles != NULL);
-            for (int y = 0; y < out_height; ++y) {
-                for (int x = 0; x < out_width; ++x) {
-                    NF_MAT_AT(NF_NN_INPUT(nn), 0, 0) = (float)x/(out_width - 1);;
-                    NF_MAT_AT(NF_NN_INPUT(nn), 0, 1) = (float)y/(out_height - 1);
-                    NF_MAT_AT(NF_NN_INPUT(nn), 0, 2) = preview_scroll;
-                    nf_nn_forward(nn);
-                    uint8_t pixel = NF_MAT_AT(NF_NN_OUTPUT(nn), 0, 0)*255.f;
-                    out_pixles[y*out_width + x] = pixel;
-                }
-            }
+            nnf_render_upscaled_screenshot(nn, preview_scroll, "number-upscaled.png");
+        }
 
-            const char *out_file_path = "number-upscaled.png";
-            if (!stbi_write_png(out_file_path, out_width, out_height, 1, out_pixles, out_width*sizeof(*out_pixles))) {
-                fprintf(stderr, "ERROR: could not write image %s\n", out_file_path);
-                return 1;
-            }
-
-            printf("Generated %s from %s\n", out_file_path, img1_file_path);
+        // render upscaled video
+        if (IsKeyPressed(KEY_X)) {
+            nnf_render_upscaled_video(nn, 5, "number-upscaled.mp4");
         }
 
         for (size_t i = 0; i < bpf && epoch < max_epoch && isRunning; ++i) {
