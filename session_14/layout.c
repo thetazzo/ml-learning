6383f34b49a07a6d0d34457069a1518a44fb28aa
pwnker <tilen.okretic@gmail.com>
2024-05-01 21:13:32 +0200

    [Layout] feat: gaps between widgets

--- session_14/layout.c
+++ session_14/layout.c
@@ -31,13 +31,15 @@ typedef struct {
     V_Layout_Orient orient;
     size_t i;
     size_t count;
+    float gap;
 } V_Layout;
 
-V_Layout nf_v_make_layout(V_Layout_Orient orient, V_Layout_Rect rect, size_t count) {
+V_Layout nf_v_make_layout(V_Layout_Orient orient, V_Layout_Rect rect, size_t count, float gap) {
     V_Layout l = {0};
     l.orient = orient;
     l.rect = rect;
     l.count = count;
+    l.gap = gap;
     return l;
 }
 
@@ -56,9 +58,12 @@ void nf_v_widget(V_Layout_Rect r, Color clr)
     DrawRectangleRec(rr, clr);
 }
 
-V_Layout_Rect nf_v_layout_slot(V_Layout *l)
+V_Layout_Rect nf_v_layout_slot(V_Layout *l, const char *file_path, int line)
 {
-    assert(l->i < l->count);
+    if (l->i >= l->count) {
+        fprintf(stderr, "%s:%d: ERROR: Layout Stack Overflow\n", file_path, line);
+        exit(1);
+    }
 
     V_Layout_Rect r = {0};
 
@@ -68,12 +73,32 @@ V_Layout_Rect nf_v_layout_slot(V_Layout *l)
             r.h = l->rect.h;
             r.x = l->rect.x + l->i*r.w;
             r.y = l->rect.y;
+
+            if (l->i == 0) { // first
+                r.w -= l->gap/2;
+            } else if (l->i >= l->count - 1) { // last
+                r.x += l->gap/2;
+                r.w -= l->gap/2;
+            } else { // middle
+                r.x += l->gap/2;
+                r.w -= l->gap;
+            }
             break;
         case V_LO_VERT:
             r.w = l->rect.w;
             r.h = l->rect.h/l->count;
             r.x = l->rect.x;
             r.y = l->rect.y + l->i*r.h;
+
+            if (l->i == 0) { // first
+                r.h -= l->gap/2;
+            } else if (l->i >= l->count - 1) { // last
+                r.y += l->gap/2;
+                r.h -= l->gap/2;
+            } else { // middle
+                r.y += l->gap/2;
+                r.h -= l->gap;
+            }
             break;
         default:
             assert(0 && "Unreachable");
@@ -99,9 +124,9 @@ typedef struct {
     size_t capacity;
 } V_Layout_Stack;
 
-void nf_v_layout_stack_push(V_Layout_Stack *ls, V_Layout_Orient orient, V_Layout_Rect rect, size_t count)
+void nf_v_layout_stack_push(V_Layout_Stack *ls, V_Layout_Orient orient, V_Layout_Rect rect, size_t count, float gap)
 {
-    V_Layout l = nf_v_make_layout(orient, rect, count);
+    V_Layout l = nf_v_make_layout(orient, rect, count, gap);
     da_append(ls, l);
 }
 
@@ -111,11 +136,13 @@ void nf_v_layout_stack_pop(V_Layout_Stack *ls)
     ls->count -= 1;
 }
 
-V_Layout_Rect nf_v_layout_stack_slot(V_Layout_Stack *ls)
+V_Layout_Rect nf_v_layout_stack_slot(V_Layout_Stack *ls, const char *file_path, int line)
 {
     assert(ls->count > 0);
-    return nf_v_layout_slot(&ls->items[ls->count-1]);
+    return nf_v_layout_slot(&ls->items[ls->count-1], file_path, line);
 }
+
+#define nf_v_layout_stack_slot(ls) nf_v_layout_stack_slot(ls, __FILE__, __LINE__)
 
 int main(void)
 {
@@ -131,21 +158,22 @@ int main(void)
     while(!WindowShouldClose()) {
         float w = GetRenderWidth();
         float h = GetRenderHeight();
-        float padding = h*0.15;
-        V_Layout_Rect root_rect = nf_v_make_layout_rect(0, padding, w, h - 2*padding);
+        float frame = h*0.15;
+        float gap = 20;
+        V_Layout_Rect root_rect = nf_v_make_layout_rect(0, frame, w, h - 2*frame);
 
         BeginDrawing();
             ClearBackground(BLACK);
-            nf_v_layout_stack_push(&ls, V_LO_HORZ, root_rect, 3);
+            nf_v_layout_stack_push(&ls, V_LO_HORZ, root_rect, 3, gap);
                 nf_v_widget(nf_v_layout_stack_slot(&ls), BLUE);
                 nf_v_widget(nf_v_layout_stack_slot(&ls), RED);
-                nf_v_layout_stack_push(&ls, V_LO_VERT, nf_v_layout_stack_slot(&ls), 3);
-                    nf_v_layout_stack_push(&ls, V_LO_HORZ, nf_v_layout_stack_slot(&ls), 2);
+                nf_v_layout_stack_push(&ls, V_LO_VERT, nf_v_layout_stack_slot(&ls), 3, gap);
+                    nf_v_layout_stack_push(&ls, V_LO_HORZ, nf_v_layout_stack_slot(&ls), 2, gap);
                         nf_v_widget(nf_v_layout_stack_slot(&ls), GREEN);
-                        nf_v_widget(nf_v_layout_stack_slot(&ls), PINK);
+                        nf_v_widget(nf_v_layout_stack_slot(&ls), GREEN);
                     nf_v_layout_stack_pop(&ls);
                     nf_v_widget(nf_v_layout_stack_slot(&ls), YELLOW);
-                    nf_v_layout_stack_push(&ls, V_LO_HORZ, nf_v_layout_stack_slot(&ls), 3);
+                    nf_v_layout_stack_push(&ls, V_LO_HORZ, nf_v_layout_stack_slot(&ls), 3, gap);
                         nf_v_widget(nf_v_layout_stack_slot(&ls), MAGENTA);
                         nf_v_widget(nf_v_layout_stack_slot(&ls), WHITE);
                         nf_v_widget(nf_v_layout_stack_slot(&ls), MAGENTA);
